74f9edca835ade433e46325912d628efa6c415b4
Kevin Smith <kevinl2code@gmail.com>
2022-01-14 11:07:56 -0600

    feat: added some code for debugging cognito

--- test/Auth/auth.test.ts
+++ test/Auth/auth.test.ts
@@ -1,6 +1,31 @@
+import AWS = require('aws-sdk')
 import { AuthService } from './AuthService'
 import { config } from './config'
 
-const authService = new AuthService()
+AWS.config.region = config.REGION
 
-const user = authService.login(config.TEST_USER_NAME, config.TEST_USER_PASSWORD)
+async function getBuckets() {
+  let buckets
+  try {
+    buckets = await new AWS.S3().listBuckets().promise()
+  } catch (error) {
+    if (error instanceof Error) buckets = undefined
+  }
+  return buckets
+}
+
+async function callStuff() {
+  const authService = new AuthService()
+
+  const user = await authService.login(
+    config.TEST_USER_NAME,
+    config.TEST_USER_PASSWORD
+  )
+  await authService.getAWSTemporaryCreds(user)
+  const someCreds = AWS.config.credentials
+  const buckets = await getBuckets()
+
+  const a = 5
+}
+
+callStuff()
