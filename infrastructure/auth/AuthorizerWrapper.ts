01e8fab48a676c994316600c2467e3eeb346dbd5
Kevin Smith <kevinl2code@gmail.com>
2022-01-13 19:09:22 -0600

    feat: added the layout for integrating cognito with the application

--- infrastructure/auth/AuthorizerWrapper.ts
+++ infrastructure/auth/AuthorizerWrapper.ts
@@ -5,8 +5,10 @@ import {
   NumberAttribute,
   UserPool,
   UserPoolClient,
+  CfnUserPoolGroup,
 } from 'aws-cdk-lib/aws-cognito'
 import { Construct } from 'constructs'
+import { IdentityPoolWrapper } from './IdentityPoolWrapper'
 
 export class AuthorizerWrapper {
   private scope: Construct
@@ -15,6 +17,7 @@ export class AuthorizerWrapper {
   private userPool: UserPool
   private userPoolClient: UserPoolClient
   public authorizer: CognitoUserPoolsAuthorizer
+  private identityPoolWrapper: IdentityPoolWrapper
 
   constructor(scope: Construct, api: RestApi) {
     this.scope = scope
@@ -26,6 +29,8 @@ export class AuthorizerWrapper {
     this.createUserPool()
     this.addUserPoolClient()
     this.createAuthorizer()
+    this.initializeIdentityPoolWrapper()
+    this.createAdminsGroup()
   }
 
   private createUserPool() {
@@ -98,5 +103,21 @@ export class AuthorizerWrapper {
       }
     )
     this.authorizer._attachToApi(this.api)
+  }
+
+  private initializeIdentityPoolWrapper() {
+    this.identityPoolWrapper = new IdentityPoolWrapper(
+      this.scope,
+      this.userPool,
+      this.userPoolClient
+    )
+  }
+
+  private createAdminsGroup() {
+    new CfnUserPoolGroup(this.scope, 'admins', {
+      groupName: 'admins',
+      userPoolId: this.userPool.userPoolId,
+      roleArn: this.identityPoolWrapper.adminRole.roleArn,
+    })
   }
 }
---
+++ test/Auth/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  REGION: 'us-east-2',
+  USER_POOL_ID: 'us-east-2_H9qQpd0lk',
+  APP_CLIENT_ID: '1ju5cu9dvsk36o2kmpq5cf76l7',
+  TEST_USER_NAME: 'testuser1',
+  TEST_USER_PASSWORD: 'Test123$',
+}
