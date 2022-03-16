883c72a3862cc1848a1e8696f07c9fd293ecee10
Kevin Smith <kevinl2code@gmail.com>
2022-03-15 19:04:10 -0500

    fix: can now delete from users table by providing PK and SK

--- services/FitnessTrackUsersTable/Delete.ts
+++ services/FitnessTrackUsersTable/Delete.ts
@@ -8,7 +8,35 @@ import { addCorsHeader } from '../Shared/Utils'
 
 const TABLE_NAME = process.env.TABLE_NAME as string
 const PRIMARY_KEY = process.env.PRIMARY_KEY as string
+const SORT_KEY = process.env.SORT_KEY
 const dbClient = new DynamoDB.DocumentClient()
+
+// async function handler(
+//   event: APIGatewayProxyEvent,
+//   context: Context
+// ): Promise<APIGatewayProxyResult> {
+//   const result: APIGatewayProxyResult = {
+//     statusCode: 200,
+//     body: 'Hello from dynamo',
+//   }
+//   addCorsHeader(result)
+
+//   const dailyEntryId = event.queryStringParameters?.[PRIMARY_KEY]
+
+//   if (dailyEntryId) {
+//     const deleteResult = await dbClient
+//       .delete({
+//         TableName: TABLE_NAME,
+//         Key: {
+//           [PRIMARY_KEY]: dailyEntryId,
+//         },
+//       })
+//       .promise()
+//     result.body = JSON.stringify(deleteResult)
+//   }
+
+//   return result
+// }
 
 async function handler(
   event: APIGatewayProxyEvent,
@@ -20,14 +48,16 @@ async function handler(
   }
   addCorsHeader(result)
 
-  const dailyEntryId = event.queryStringParameters?.[PRIMARY_KEY]
+  const PK = event.queryStringParameters?.[PRIMARY_KEY]
+  const SK = event.queryStringParameters?.[SORT_KEY!]
 
-  if (dailyEntryId) {
+  if (PK && SK) {
     const deleteResult = await dbClient
       .delete({
         TableName: TABLE_NAME,
         Key: {
-          [PRIMARY_KEY]: dailyEntryId,
+          [PRIMARY_KEY]: PK,
+          [SORT_KEY!]: SK,
         },
       })
       .promise()
